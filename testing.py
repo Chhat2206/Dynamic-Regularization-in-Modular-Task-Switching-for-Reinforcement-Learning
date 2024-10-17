20362bd4b962fd888875b705b2df22c54a017569
Chhat2206 <chhatseh@gmail.com>
2024-10-17 20:47:20 +0800

    Commits just in case

--- testing.py
+++ testing.py
@@ -1,201 +1,88 @@
-# Adapted from: https://medium.com/@tuzzer/cart-pole-balancing-with-q-learning-b54c6068d947
-
 import gymnasium as gym
 import numpy as np
-import random
-import math
-import time
-# from time import sleep
-import numpy as np
-import random
-import math
-from time import sleep
 
-## Initialize the "Cart-Pole" environment
-env = gym.make("CartPole-v1", render_mode="human")
-env.metadata['render_fps'] = 240
-env._max_episode_steps = 200
+# Create the CartPole environment using Gymnasium
+env = gym.make('CartPole-v1')
 
-# Initializing the random number generator
-np.random.seed(int(time.time()))
+# Parameters
+switch_threshold = 10  # Step threshold to switch reward structures
+noise_factor = 0.1  # Noise level for observation noise injection
+num_episodes = 100  # Increased number of episodes to run
 
-## Defining the environment related constants
+# Initializing the Q-table: (number of states, number of actions)
+# Cartpole doesn't have a discrete observation space, so this is recommended discreting metrics
+# Number of discrete states for each dimension (position, velocity, angle, angular velocity)
+n_bins = (6,12)
+q_table = np.zeros([env.observation_space.shape[0], env.action_space.shape.n])
+# env.observation_space = Searches the entire search space of the enviorment
+# In cartpole, the state consists of four continuous values: the cart’s position,
+# cart’s velocity, pole angle, and pole's angular velocity. So it will return 4
+# env.action_space.n = Actions agent can take, so 2 (left & right)
 
-# Number of discrete states (bucket) per state dimension
-NUM_BUCKETS = (1, 1, 6, 3)  # (x, x', theta, theta')
-# Number of discrete actions
-NUM_ACTIONS = env.action_space.n  # (left, right)
-# Bounds for each discrete state
-STATE_BOUNDS = list(zip(env.observation_space.low, env.observation_space.high))
-STATE_BOUNDS[1] = (-0.5, 0.5)
-STATE_BOUNDS[3] = (-math.radians(50), math.radians(50))
-# Index of the action
-ACTION_INDEX = len(NUM_BUCKETS)
+# Making continous values into discrete
+def discretize_states(state, bins):
+    # high gives the upper values of the state
+    # = [gives highest possible value], 0.5 (all values above 0.5 are treated as 0.5 to avoid screwing over the table)
+    # cart position, cart velocity pole angle, pole angle velocity
+    upper_bounds = [env.observation_space.high[0], 0.5, env.observation_space.high[2], 0.5]
+    lower_bounds = [env.observation_space.low[0], -0.5, env.observation_space.low[2], -0.5]
 
-## Creating a Q-Table for each state-action pair
-q_table = np.zeros(NUM_BUCKETS + (NUM_ACTIONS,))
+    # Current value of the state [cart_position, cart_velocity, pole_angle, pole_angular_velocity]
+    # lower_bounds = min value, upper_bounds = max value
+    # abs turns it positive for normalization
+    # then state[i] makes the bound from 0 0.5 and 1
+    # each state (1/4) has its own lower and upper bounds to run into this formula
+    ratios = [(state[i] + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i in range(len(state))]
 
-## Learning related constants
-MIN_EXPLORE_RATE = 0.01
-MIN_LEARNING_RATE = 0.1
-TEST_RAND_PROB = 0.2
+    new_state = [int(round((bin[i] - 1) * ratios[i])) for i in range(len(state))]
 
-## Defining the simulation related constants
-NUM_TRAIN_EPISODES = 180  # 1000
-NUM_TEST_EPISODES = 1
-MAX_TRAIN_T = 250
-MAX_TEST_T = 250
-STREAK_TO_END = 10  # 120
-SOLVED_T = 199
-VERBOSE = False
+# Function to inject noise into the observation
+def inject_noise(state, noise_factor):
+    noise = np.random.randn(*state.shape) * noise_factor
+    return state + noise  # Add noise to the state observations
 
+# Running multiple episodes
+for episode in range(num_episodes):
+    # Reset the environment to start a new episode
+    state, info = env.reset()
 
-def train():
-    ## Instantiating the learning related parameters
-    learning_rate = get_learning_rate(0)
-    explore_rate = get_explore_rate(0)
-    discount_factor = 0.99  # since the world is unchanging
+    total_reward = 0  # Track total reward in each episode
+    adjusted_reward = 0  # Initialize the adjusted reward
 
-    num_train_streaks = 0
+    for step in range(5000000):  # Increased to 500 steps per episode
+        # Inject noise into the observations (optional)
+        noisy_state = inject_noise(state, noise_factor)
 
-    for episode in range(NUM_TRAIN_EPISODES):
+        # Take a random action (you can replace this with an RL agent's action)
+        action = env.action_space.sample()
 
-        # Reset the environment
-        obv, info = env.reset()
+        # Perform the action in the environment
+        state, reward, done, truncated, info = env.step(action)
 
-        # the initial state
-        state_0 = state_to_bucket(obv)
+        # Task switching based on the step count (rewards change after switch_threshold steps)
+        if step < switch_threshold:
+            # Task 1: Use the standard reward (no changes)
+            adjusted_reward = reward
+        else:
+            # Task 2: Modify the reward based on the pole's angle (state[2] is the pole angle)
+            pole_angle = state[2]  # Get the pole angle from the state
+            if abs(pole_angle) < 0.1:
+                # Give a higher reward if the pole is near upright
+                adjusted_reward = 2 * reward
+            else:
+                # Penalize if the pole is at a steep angle (about to fall)
+                adjusted_reward = reward - 1
 
-        for t in range(MAX_TRAIN_T):
-            env.render()
+        # Accumulate the adjusted reward for the episode
+        total_reward += adjusted_reward
 
-            # Select an action
-            action = select_action(state_0, explore_rate)
+        # Print the current step, original reward, and adjusted reward
+        print(f"Episode {episode+1}, Step: {step}, Reward: {reward}, Adjusted Reward: {adjusted_reward}")
 
-            # Execute the action
-            obv, reward, terminated, truncated, info = env.step(action)
-
-            # Observe the result
-            state = state_to_bucket(obv)
-
-            # Update the Q based on the result
-            best_q = np.amax(q_table[state])
-            q_table[state_0 + (action,)] += learning_rate * (
-                        reward + discount_factor * (best_q) - q_table[state_0 + (action,)])
-
-            # Setting up for the next iteration
-            state_0 = state
-
-            # Print data
-            if (VERBOSE):
-                print("\nEpisode = %d" % episode)
-                print("t = %d" % t)
-                print("Action: %d" % action)
-                print("State: %s" % str(state))
-                print("Reward: %f" % reward)
-                print("Best Q: %f" % best_q)
-                print("Explore rate: %f" % explore_rate)
-                print("Learning rate: %f" % learning_rate)
-                print("Streaks: %d" % num_train_streaks)
-
-                print("")
-
-            if terminated or truncated:
-                print("Episode %d finished after %f time steps" % (episode, t))
-                if (t >= SOLVED_T):
-                    num_train_streaks += 1
-                else:
-                    num_train_streaks = 0
-                break
-
-            # sleep(0.25)
-
-        # It's considered done when it's solved over 120 times consecutively
-        if num_train_streaks > STREAK_TO_END:
+        # Check if the episode is done (if the pole fell or the max steps were reached)
+        if done or truncated:
+            print(f"Episode {episode+1} finished after {step+1} steps with total reward: {total_reward}")
             break
 
-        # Update parameters
-        explore_rate = get_explore_rate(episode)
-        learning_rate = get_learning_rate(episode)
-
-
-def test():
-    num_test_streaks = 0
-
-    for episode in range(NUM_TEST_EPISODES):
-
-        # Reset the environment
-        obv, info = env.reset()
-
-        # the initial state
-        state_0 = state_to_bucket(obv)
-
-        # basic initializations
-        tt = 0
-        terminated = False
-
-        while ((abs(obv[0]) < 2.4) & (abs(obv[2]) < 45)):
-            # while not(terminated):
-            tt += 1
-            env.render()
-
-            # Select an action
-            action = select_action(state_0, 0)
-            # action = select_action(state_0, TEST_RAND_PROB)
-            # action = select_action(state_0, 0.01)
-
-            # Execute the action
-            obv, reward, terminated, truncated, info = env.step(action)
-
-            # Observe the result
-            state_0 = state_to_bucket(obv)
-
-            print("Test episode %d; time step %f." % (episode, tt))
-
-
-def select_action(state, explore_rate):
-    # Select a random action
-    if random.random() < explore_rate:
-        action = env.action_space.sample()
-    # Select the action with the highest q
-    else:
-        action = np.argmax(q_table[state])
-    return action
-
-
-def get_explore_rate(t):
-    return max(MIN_EXPLORE_RATE, min(1, 1.0 - math.log10((t + 1) / 25)))
-
-
-def get_learning_rate(t):
-    return max(MIN_LEARNING_RATE, min(0.5, 1.0 - math.log10((t + 1) / 25)))
-
-
-def state_to_bucket(state):
-    bucket_indice = []
-    for i in range(len(state)):
-        if state[i] <= STATE_BOUNDS[i][0]:
-            bucket_index = 0
-        elif state[i] >= STATE_BOUNDS[i][1]:
-            bucket_index = NUM_BUCKETS[i] - 1
-        else:
-            # Mapping the state bounds to the bucket array
-            bound_width = STATE_BOUNDS[i][1] - STATE_BOUNDS[i][0]
-            offset = (NUM_BUCKETS[i] - 1) * STATE_BOUNDS[i][0] / bound_width  # "index per boudns" * min_bound
-            scaling = (NUM_BUCKETS[i] - 1) / bound_width  # how much index per width
-            bucket_index = int(round(scaling * state[i] - offset))
-            # For easier visualization of the above, you might want to use
-            # pen and paper and apply some basic algebraic manipulations.
-            # If you do so, you will obtaint (B-1)*[(S-MIN)]/W], where
-            # B = NUM_BUCKETS, S = state, MIN = STATE_BOUNDS[i][0], and
-            # W = bound_width. This simplification is very easy to
-            # to visualize, i.e. num_buckets x percentage in width.
-        bucket_indice.append(bucket_index)
-    return tuple(bucket_indice)
-
-
-if __name__ == "__main__":
-    print('Training ...')
-    train()
-    print('Testing ...')
-    test()
+# Close the environment after running all episodes
+env.close()
